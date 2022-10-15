1c223933db145ce4dc5907912106596cb0176a72
yura178500 <yura178500@yandex.ru>
2022-10-15 20:15:25 +0300

    задача 6.2 (1-4) Исправил

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,38 @@
 public class Main {
     public static void main(String[] args) {
         //Задача 1
-        int solary = 15000;
+        int salary = 15000;
         int total = 0;
         for (int i = 0; total < 12000000; i++) {
-            total = total + total * 0.7;
-            total = total + solary;
+            total = (int) (total + total * 0.7);
+            total = total + salary;
             System.out.print("Месяц" + i + "Доход" + total);
         }
 
         // Задача 2
-        int solary = 15000;
+        int salary = 15000;
         int total = 0;
         for (int i = 0; total < 12000000; i++) {
-            i = i + 6:
-            total = total + total * 0.7;
-            total = total + solary;
-            System.out.print("Месяц" + i + "Доход" + total);
-        }
+            total = (int) (total + total * 0.7);
+            total = total + salary;
+            if(i % 6 == 0) { System.out.print("Месяц" + i + "Доход" + total); }
+
 
         // Задача 3
-        int solary = 15000;
+        int salary = 15000;
         int total = 0;
         while (total < 12000000) {
-            for (int i = 0; i < 9; i++) {
+            for (int i = 0; i <= 9; i++) {
                 i = i / 2;
-                total = total + total * 0.7;
-                total = total + solary;
+                total = (int) (total + total * 0.7);
+                total = total + salary;
                 System.out.print("Месяц" + i + "Доход" + total);
             }
 
             //Задача 4
             int monthNumber = 1;
+            int friday = monthNumber + 7;
             while (monthNumber < 31) {
-                int friday = monthNumber + 7;
                 System.out.print("Сегодня" + friday + "пятница, необходимо подготовить отчет.");
             }
         }
