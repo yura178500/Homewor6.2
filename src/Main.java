8f70ef52f08a92eb0c68a3575e751a3e4530d134
yura178500 <yura178500@yandex.ru>
2022-10-16 13:32:57 +0300

    задача 6.2 (1-4) Исправил

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,10 @@ public class Main {
         for (int i = 0; total < 12000000; i++) {
             total = (int) (total + total * 0.7);
             total = total + salary;
-            if(i % 6 == 0) { System.out.print("Месяц" + i + "Доход" + total); }
+            if (i % 6 == 0) {
+                System.out.print("Месяц" + i + "Доход" + total);
+            }
+        }
 
 
         // Задача 3
@@ -28,12 +31,12 @@ public class Main {
                 total = total + salary;
                 System.out.print("Месяц" + i + "Доход" + total);
             }
-
-            //Задача 4
-            int monthNumber = 1;
-            int friday = monthNumber + 7;
-            while (monthNumber < 31) {
-                System.out.print("Сегодня" + friday + "пятница, необходимо подготовить отчет.");
-            }
         }
-    }}
+
+        //Задача 4
+        int monthNumber = 1;
+        while (monthNumber < 31) {
+            System.out.print("Сегодня" + monthNumber + "пятница, необходимо подготовить отчет.");
+            int monthNumber = monthNumber + 7;
+        }
+    }
